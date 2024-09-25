441aeeeb129c731ef66728b46db4bd13e46a4b48
Дима Бучнев <buchnev.dima@mail.ru>
2024-09-25 17:52:34 +0300

    Add plagin whith integration in Revit

--- Alg.cs
+++ Alg.cs
@@ -1,7 +1,47 @@
-﻿namespace Transformation2Dto3DLib
-{
-    public class Alg
-    {
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
 
+
+namespace Transformation2Dto3DLib
+{
+
+public class SegmentTo3D : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ElementSet elements)
+    {
+        // Путь к сегментированному изображению
+        string segmentedImagePath = "path_to_your_segmented_image.png";
+        
+        // Загружаем изображение
+        Bitmap bitmap = new Bitmap(segmentedImagePath);
+        
+        // Создайте новую транзакцию
+        using (Transaction transaction = new Transaction(commandData.Application.ActiveUIDocument.Document, "Create 3D Model"))
+        {
+            transaction.Start();
+
+            // Простой пример создания 3D-объекта на основе сегментации
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    System.Drawing.Color pixelColor = bitmap.GetPixel(x, y);
+                    if (pixelColor.R == 255) // Проверка белого пикселя
+                    {
+                        // Создание куба или другого 3D-объекта в Revit
+                        XYZ point = new XYZ(x, y, 0);
+                        // Далее добавьте код для создания объекта, например, стены или колонны
+                    }
+                }
+            }
+
+            transaction.Commit();
+        }
+
+        return Result.Succeeded;
     }
+}
 }
